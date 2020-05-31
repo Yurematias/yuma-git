35e39b50a006d6bba789d876deca83af6e5ad666
yure <yurematias@ufrn.edu.br>
2020-05-31 01:59:56 -0300

    bugs fixed

--- yuma.sh
+++ yuma.sh
@@ -4,9 +4,9 @@ COMMIT_MESSAGE="$2"
 if [ "${#FILES}" -gt 0 ]; then
 	git add "$FILES"
 	if [ "${#FILES}" -gt 0 ]; then
-		git commit -m "update"
-	else
 		git commit -m "$COMMIT_MESSAGE"
+	else
+		git commit -m "update"
 	fi
 		git push
 else
